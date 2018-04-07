675bd421615cba2698757d35b031c70219b301d6
ibragimov <ibragimov@skbkontur.ru>
2018-04-08 01:34:45 +0500

    removed TestLevel

--- src/models/LevelFactory.js
+++ src/models/LevelFactory.js
@@ -142,49 +142,6 @@ function LevelFactory(levelName) {
 		return level;
 	}
 
-	function TestLevel() {
-		var cities = [];
-
-		var moscow = new City("Москва", 1000, 100, 70, 0.7);
-		var spb = new City("Санкт-Петербург", 1000, 10, 20, 0.2);
-
-		var kazan = new City("Казань", 1000, 300, 100, 0.6);
-		var rostov = new City("Ростов", 1000, 100, 400, 0.2);
-
-		cities.push(moscow);
-		cities.push(spb);
-		cities.push(kazan);
-		cities.push(rostov);
-
-		var transports = [];
-
-		var roads = [];
-		roads.push(new Road(moscow, spb));
-	    roads.push(new Road(moscow, kazan));
-	    roads.push(new Road(spb, rostov));
-	    roads.push(new Road(rostov, kazan));
-
-		var map = {};
-		map[moscow.name] = addEdge(moscow, [spb, kazan]);
-		map[spb.name] = addEdge(spb, [moscow, rostov]);
-		map[kazan.name] = addEdge(kazan, [moscow, rostov]);
-		map[rostov.name] = addEdge(rostov, [spb, kazan]);
-
-
-		var graph = new Graph(map, cities);
-
-		var level = new Level(cities, roads, transports, 1000, 100, graph);
-		
-		var transport = new Transport("Car", 10, truckVelocity, graph, level);
-		transport.currentCity = spb;
-
-		transport.position = 0;
-
-		transports.push(transport);
-
-		return level;
-	}
-
 	function addEdge(mainCity, cities){
 		var fromMainCity = {};
 		for (var i = 0; i < cities.length; i++){
